ebee19b227209523236368da72f105ff3d891640
SlawomirDudziak <50486478+SlawomirDudziak@users.noreply.github.com>
2021-12-20 18:51:23 +0100

    add movie, tvshow details

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -13,11 +13,13 @@ class MoviesController extends AbstractController {
     private $movies_url = "https://api.themoviedb.org/3/movie/upcoming?api_key=";
     private $tvshows_url = "https://api.themoviedb.org/3/trending/tv/week?api_key=";
     private $person_url = "https://api.themoviedb.org/3/trending/person/week?api_key=";
+    public $movies_table = array();
 
     public function __construct(HttpClientInterface $client, $api_key)
     {
         $this->client = $client;
         $this->api_key = $api_key;
+        // $this->movies_table = $movies_table;
     }
 
     public function fetchTrendingMovies()
@@ -28,8 +30,9 @@ class MoviesController extends AbstractController {
             $api_url
         );
 
-        $content = $response->toArray();
-        return $content;
+        // $content = $response->toArray();
+        // return $content;
+        return $response->toArray();
     }
 
     public function fetchTrendingTVShows()
@@ -51,8 +54,14 @@ class MoviesController extends AbstractController {
             $api_url
         );
 
-        $content = $response->toArray();
-        return $content;
+        // $content = $response->toArray();
+        // return $content;
+        return $response->toArray();
+    }
+
+    public function getMovies()
+    {
+        return $this->movies_table;
     }
 
     /**
@@ -70,10 +79,30 @@ class MoviesController extends AbstractController {
     {
         $movies = array();
         $movies = $this->fetchTrendingMovies();
-
+        
+        // $this->movies_table = $movies;
+        $this->movies_table = $movies;
+        
         return $this->render('movies/movies.html.twig', $movies);
     }
-
+    
+    /**
+     * @Route("/movies/{id}", name="movie_details")
+     */
+    public function movie($id)
+    {
+        $movies = array();
+        // $movies = $this->movies_table;
+        $movies = $this->fetchTrendingMovies();
+        // print_r($this->movies_table);
+        $details = '';
+        for ($i = 0; $i < 20; $i++) {
+            if ($id == $movies['results'][$i]['id']) $details = $movies['results'][$i];
+        }
+        
+        return $this->render('movies/movie_details.html.twig', array('details' => $details));
+    }
+    
     /**
      * @Route("/tvshows", name="tvshows_list")
      */
@@ -83,6 +112,21 @@ class MoviesController extends AbstractController {
         $tvshows = $this->fetchTrendingTVShows();
 
         return $this->render('tvshows/tvshows.html.twig', $tvshows);
+    }
+
+    /**
+     * @Route("/tvshows/{id}", name="tvshows_details")
+     */
+    public function tvshow($id)
+    {
+        $tvshows = array();
+        $tvshows = $this->fetchTrendingTVShows();
+        $details = '';
+        for ($i = 0; $i < 20; $i++) {
+            if ($id == $tvshows['results'][$i]['id']) $details = $tvshows['results'][$i];
+        }
+        
+        return $this->render('tvshows/tvshow_details.html.twig', array('details' => $details));
     }
 
     /**
