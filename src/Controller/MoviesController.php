85dc2fbfb4af9ff8f88bf10ddf2140264fcbc0fb
SlawomirDudziak <50486478+SlawomirDudziak@users.noreply.github.com>
2021-12-21 17:52:09 +0100

    add top rated movies/tvshows

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -11,9 +11,11 @@ class MoviesController extends AbstractController {
     private $client;
     private $api_key;
     private $movies_url = "https://api.themoviedb.org/3/movie/upcoming?api_key=";
+    private $top_rated_movies_url = "https://api.themoviedb.org/3/movie/top_rated?api_key=";
     private $tvshows_url = "https://api.themoviedb.org/3/trending/tv/week?api_key=";
+    private $top_rated_tvshows_url = "https://api.themoviedb.org/3/tv/top_rated?api_key=";
     private $person_url = "https://api.themoviedb.org/3/trending/person/week?api_key=";
-    public $movies_table = array();
+    // public $movies_table = array();
 
     public function __construct(HttpClientInterface $client, $api_key)
     {
@@ -30,14 +32,34 @@ class MoviesController extends AbstractController {
             $api_url
         );
 
-        // $content = $response->toArray();
-        // return $content;
+        return $response->toArray();
+    }
+
+    public function fetch_top_rated_movies()
+    {
+        $api_url = "{$this->top_rated_movies_url}{$this->api_key}";
+        $response = $this->client->request(
+            'GET',
+            $api_url
+        );
+
         return $response->toArray();
     }
 
     public function fetchTrendingTVShows()
     {
         $api_url = "{$this->tvshows_url}{$this->api_key}";
+        $response = $this->client->request(
+            'GET',
+            $api_url
+        );
+
+        return $response->toArray();
+    }
+
+    public function fetch_top_rated_tvshows()
+    {
+        $api_url = "{$this->top_rated_tvshows_url}{$this->api_key}";
         $response = $this->client->request(
             'GET',
             $api_url
@@ -54,8 +76,6 @@ class MoviesController extends AbstractController {
             $api_url
         );
 
-        // $content = $response->toArray();
-        // return $content;
         return $response->toArray();
     }
 
@@ -73,16 +93,23 @@ class MoviesController extends AbstractController {
     }
 
     /**
-     * @Route("/movies", name="movies_list")
+     * @Route("/movies/upcoming", name="upcoming_movies")
      */
     public function movies()
     {
         $movies = array();
         $movies = $this->fetchTrendingMovies();
-        
-        // $this->movies_table = $movies;
-        $this->movies_table = $movies;
-        
+                
+        return $this->render('movies/movies.html.twig', $movies);
+    }
+    
+    /**
+     * @Route("/movies/toprated", name="top_rated_movies")
+     */
+    public function top_rated_movies()
+    {
+        $movies = $this->fetch_top_rated_movies();
+
         return $this->render('movies/movies.html.twig', $movies);
     }
     
@@ -92,9 +119,7 @@ class MoviesController extends AbstractController {
     public function movie($id)
     {
         $movies = array();
-        // $movies = $this->movies_table;
         $movies = $this->fetchTrendingMovies();
-        // print_r($this->movies_table);
         $details = '';
         for ($i = 0; $i < 20; $i++) {
             if ($id == $movies['results'][$i]['id']) $details = $movies['results'][$i];
@@ -104,12 +129,22 @@ class MoviesController extends AbstractController {
     }
     
     /**
-     * @Route("/tvshows", name="tvshows_list")
+     * @Route("/tvshows/upcoming", name="upcoming_tvshows")
      */
     public function tvshows()
     {
         $tvshows = array();
         $tvshows = $this->fetchTrendingTVShows();
+
+        return $this->render('tvshows/tvshows.html.twig', $tvshows);
+    }
+
+    /**
+     * @Route("/tvshows/toprated", name="top_rated_tvshows")
+     */
+    public function top_rated_tvshows()
+    {
+        $tvshows = $this->fetch_top_rated_tvshows();
 
         return $this->render('tvshows/tvshows.html.twig', $tvshows);
     }
@@ -130,7 +165,7 @@ class MoviesController extends AbstractController {
     }
 
     /**
-     * @Route("/people", name="people_list")
+     * @Route("/people/popular", name="popular_people")
      */
     public function people()
     {
